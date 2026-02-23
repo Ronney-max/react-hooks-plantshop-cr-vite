e67dfe088518861fe7f8eef7ffb837b0f7800084
ronney omondi <ronneyomondi14@gmail.com>
2026-02-23 11:21:46 +0300

    initial commit

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-
-function PlantCard() {
+function PlantCard({plant, onUpdatePlant}) {
+  function handleSoldOut() {
+    onUpdatePlant({...plant, soldOut: !plant.soldOut})
+  }
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
+      <img src={"http://127.0.0.1:3000/plants"} alt={"plant name"} />
       <h4>{"plant name"}</h4>
       <p>Price: {"plant price"}</p>
       {true ? (
-        <button className="primary">In Stock</button>
+        <button onClick={handleSoldOut}>
+          {plant.soldOut ? "In Stock" : "Sold Out"}
+        </button>
       ) : (
         <button>Out of Stock</button>
       )}
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,64 @@
-import React from "react";
+import  { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // ✅ GET plants on page load
+  useEffect(() => {
+    fetch("http://127.0.0.1:3000/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  // ✅ Add new plant (POST)
+  function handleAddPlant(newPlant) {
+    fetch("http://127.0.0.1:3000/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPlant),
+    })
+      .then((res) => res.json())
+      .then((data) => setPlants([...plants, data]));
+  }
+
+  // ✅ Mark plant as sold out (PATCH)
+  function handleUpdatePlant(updatedPlant) {
+    fetch(`http://127.0.0.1:3000/plants/${updatedPlant.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedPlant),
+    })
+      .then((res) => res.json())
+      .then((data) =>
+        setPlants(
+          plants.map((plant) =>
+            plant.id === data.id ? data : plant
+          )
+        )
+      );
+  }
+
+  // ✅ Search Filter
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <PlantList
+        plants={filteredPlants}
+        onUpdatePlant={handleUpdatePlant}
+      />
     </main>
   );
 }
